5406e5409b8a5f9f18e2c06b9642376fecb904ab
dcollins4096 <none@none>
2019-04-26 10:34:17 -0400

    adding go and GL for all your startup needs.

---
+++ go.py
@@ -0,0 +1,18 @@
+
+import matplotlib
+matplotlib.use('Agg')
+import yt
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+import pdb
+nar = np.array
+from importlib import reload
+import looper
+reload(looper)
+import trackage
+reload(trackage)
+import pickle
+#from core_dump import *
+from scipy.stats import kurtosis
+from scipy.stats import skew
