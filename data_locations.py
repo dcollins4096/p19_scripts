579842f243458a6b3967e66d79dcf68ffe1b129d
dcollins4096 <none@none>
2019-06-07 13:38:53 -0400

    adding data_location

---
+++ data_locations.py
@@ -0,0 +1,7 @@
+
+enzo_directory = '/scratch2/dcollins/Paper19_48/B02/u05-r4-l4-128'
+snapshot_location ='/scratch1/dcollins/Paper19/Datasets/track_indfix_sixteenframe'
+if 'machine' in os.environ:
+    if os.environ['machine'] in ['shipsterns']:
+        enzo_directory = '/home/dcollins/scratch/u05-r4-l4-128'
+        snapshot_location = '/home/dcollins/scratch/Paper19/track_index_fix'
