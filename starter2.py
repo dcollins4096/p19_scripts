1bb6c068fcf6be6b41de5b2f8cc2368f47908aee
dcollins4096 <none@none>
2019-06-07 12:31:27 -0400

    rebranding

---
+++ starter2.py
@@ -0,0 +1,17 @@
+from starter1 import *
+import looper
+reload(looper)
+import trackage
+reload(trackage)
+#from core_dump import *
+import yt
+
+directory = '/home/dcollins/scratch/u05-r4-l4-128'
+
+import looper
+reload(looper)
+import trackage
+reload(trackage)
+import tracks_read_write as trw
+reload(trw)
+from davetools import *
